376ef87bd0d6a529c4e9df74903cfa225ec363f2
arahman225 <upworkabdurrahman@gmail.com>
2024-12-22 09:12:37 +0600

    added special offer component

--- src/Components/WhyChoose.jsx
+++ src/Components/WhyChoose.jsx
@@ -27,26 +27,26 @@ const WhyChooseUs = () => {
     ];
 
     return (
-        <section className="bg-[#F5F6FE] py-16">
+        <section className="bg-[#F5F6FE] py-20">
             <div className="max-w-7xl mx-auto px-6 lg:px-8">
-                <div className="text-center md:w-4/6 mx-auto py-10">
+                <div className="text-center md:w-4/6 mx-auto py-16">
                     <h2 className="text-3xl font-bold text-[#FF3600] text-center">Why Choose Us?</h2>
                     <p className="text-5xl font-bold">Save money with competitive daily and weekly pricing</p>
                 </div>
-                <div className="bg-cover bg-center bg-no-repeat " style={{ backgroundImage: `url(${chooseImg})` }}>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 " >
+                
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-16" >
                     {features.map((feature, index) => (
                         <div
                             key={index}
-                            className="flex flex-col items-center bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow"
+                            className="flex flex-col items-center bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow border"
                         >
                             <div className="mb-4">{feature.icon}</div>
-                            <h3 className="text-xl font-semibold text-gray-800 mb-2">{feature.title}</h3>
+                            <h3 className="text-xl font-semibold mb-2 text-[#FF3600]">{feature.title}</h3>
                             <p className="text-gray-600 text-center">{feature.description}</p>
                         </div>
                     ))}
                 </div>
-                </div>
+                
             </div>
         </section>
     );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Carousel from '../Components/Carosol';
 import WhyChooseUs from '../Components/WhyChoose';
+import SpecialOffers from '../Components/SpecailOffer';
 
 const Home = () => {
     return (
         <div>
             <Carousel></Carousel>
             <WhyChooseUs></WhyChooseUs>
+            <SpecialOffers></SpecialOffers>
         </div>
     );
 };
